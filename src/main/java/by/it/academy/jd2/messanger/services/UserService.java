9746b749e69a09adae8c9c4a71b138f68c30711d
Burukku1 <dima.dmitry06@gmail.com>
2023-09-25 00:04:08 +0300

    realize UserService to save new users

--- src/main/java/by/it/academy/jd2/messanger/services/UserService.java
+++ src/main/java/by/it/academy/jd2/messanger/services/UserService.java
@@ -1,14 +1,47 @@
 package by.it.academy.jd2.messanger.services;
 
-import by.it.academy.jd2.messanger.User;
+import by.it.academy.jd2.messanger.domain.User;
 import by.it.academy.jd2.messanger.core.exeptions.ValidationException;
+import by.it.academy.jd2.messanger.repository.api.IUserRepo;
 import by.it.academy.jd2.messanger.services.api.IUserService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class UserService implements IUserService {
 
-    // private Dao
+    private IUserRepo userRepo;
+
+    private List<Character> illegalSymbols = new ArrayList<>
+            (Arrays.asList('<', '>', '!', '@', '`', '.', ',', ';', ':', '\'', '\"'));
     @Override
     public void save(User user) throws ValidationException {
-        //todo save in dao
+        if(isIllegalData(user)){
+            userRepo.saveUser(user);
+        } else {
+            throw new ValidationException("Некорректный ввод");
+        }
+    }
+
+    public boolean isIllegalData(User user) throws ValidationException{
+        if (user.getLogin() == null || user.getPassword() == null){
+            return false;
+        }
+        for (char c : illegalSymbols){
+            if (user.getLogin().equals(c)){
+                return false;
+            }
+            if (user.getPassword().equals(c)){
+                return false;
+            }
+            if (user.getFio().equals(c)){
+                return false;
+            }
+            if (user.getRole().equals(c)){
+                return false;
+            }
+        }
+        return true;
     }
 }

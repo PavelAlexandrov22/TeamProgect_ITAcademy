6d55bfb341f7b3a9afd54a866347bf68c6ac4336
Burukku1 <dima.dmitry06@gmail.com>
2023-09-25 00:16:49 +0300

    update UserServiceFactory

---
+++ src/main/java/by/it/academy/jd2/messanger/services/factory/UserServiceFactory.java
@@ -0,0 +1,26 @@
+package by.it.academy.jd2.messanger.services.factory;
+
+
+import by.it.academy.jd2.messanger.repository.UserRepo;
+import by.it.academy.jd2.messanger.services.LoginService;
+import by.it.academy.jd2.messanger.services.UserService;
+
+import by.it.academy.jd2.messanger.services.api.IUserService;
+
+public class UserServiceFactory {
+    private volatile static UserService instance;
+
+    private UserServiceFactory() {
+    }
+
+    public static IUserService getInstance() {
+        if(instance == null){
+            synchronized (UserServiceFactory.class){
+                if(instance == null){
+                    instance = new UserService(new UserRepo());
+                }
+            }
+        }
+        return instance;
+    }
+}

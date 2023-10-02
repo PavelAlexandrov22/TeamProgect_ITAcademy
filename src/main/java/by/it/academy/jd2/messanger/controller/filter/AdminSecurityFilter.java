426c7ab5571e8b10d03f3c18e37793bcc1c48bf0
alexa <alexandrov-pasha@yandex.ru>
2023-10-02 14:15:26 +0300

    up filter

--- src/main/java/by/it/academy/jd2/messanger/controller/filter/AdminSecurityFilter.java
+++ src/main/java/by/it/academy/jd2/messanger/controller/filter/AdminSecurityFilter.java
@@ -3,14 +3,16 @@ package by.it.academy.jd2.messanger.controller.filter;
 
 import by.it.academy.jd2.messanger.domain.User;
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-
+@WebFilter(urlPatterns =  "/admin/login")
 public class AdminSecurityFilter implements Filter {
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
@@ -23,11 +25,9 @@ public class AdminSecurityFilter implements Filter {
             user.setRole("admin");
             if(user.getRole().equals("admin")){
                 chain.doFilter(request, response);
-            }else {
-                resp.sendRedirect(contextPath + "/");
             }
         }else{
-            resp.sendRedirect(contextPath + "/ui/signup.jsp");
+            resp.sendRedirect(contextPath + "/admin/statistic");
         }
 
 
--- src/main/java/by/it/academy/jd2/messanger/controller/filter/UserSecurityFilter.java
+++ src/main/java/by/it/academy/jd2/messanger/controller/filter/UserSecurityFilter.java
@@ -1,13 +1,14 @@
 package by.it.academy.jd2.messanger.controller.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-
+@WebFilter(urlPatterns = "/api/message")
 public class UserSecurityFilter implements Filter {
 
     @Override
